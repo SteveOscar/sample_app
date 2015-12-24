270d786e253866e693f89d3cea2ec308e39cabd6
Steven Olson <steveoscaro@gmail.com>
2015-12-24 12:51:59 -0700

    finished user signup

---
+++ test/integration/users_signun_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersSignunTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Rails Tutorial",
+                               email: "example@rails.org",
+                               password:              "aabtpwd",
+                               password_confirmation: "aabtpwd" }
+    end
+    assert_template 'users/show'
+  end
+end
