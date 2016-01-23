de8bc2e6ea8aeedfe1ed12dd85a9221f3f6dcf25
Steven Olson <steveoscaro@gmail.com>
2016-01-22 17:25:36 -0700

    half way through 8, all tests passing

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,18 @@
 module SessionsHelper
 
-  #logs the user in
+  # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -17,26 +25,21 @@ module SessionsHelper
     end
   end
 
-  def rememeber(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent.signed[:rememeber_token] = user.remember_token
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
   end
 
+  # Forgets a persisiten session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
-
-  def logged_in?
-    !current_user.nil?
-  end
-
+  # Logs out the current user.
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
-
 end
